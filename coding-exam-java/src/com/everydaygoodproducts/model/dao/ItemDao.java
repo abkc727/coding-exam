ed238873f3ea2ecae59ebf01237564055a9df60d
Aditya Babu K C <71261653+abkc727@users.noreply.github.com>
2020-09-24 17:50:35 +0530

    Coding exam

--- coding-exam-java/src/com/everydaygoodproducts/model/dao/ItemDao.java
+++ coding-exam-java/src/com/everydaygoodproducts/model/dao/ItemDao.java
@@ -6,16 +6,9 @@ import com.everydaygoodproducts.model.beans.Item;
 
 public interface ItemDao {
 	
-	public Item storeFood(Item item);
+	public Item store(Item item);
 	public Item[] fetchItems();
 	public Item fetchItemByCode(int itemCode);
-	
-	public Item storeApparel(Item item);
-	public Item[] fetchItems();
-	public Item fetchItemByCode(int itemCode)
-	
-	public Item storeElectronics(Item item);
-	public Item[] fetchItems();
-	public Item fetchItemByCode(int itemCode)
+
 
 }
